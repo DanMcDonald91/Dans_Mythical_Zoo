5c96a938a1f58364ee242f1dfcbe3e5b36523cfc
Daniel McDonald <danielkg91@gmail.comssh-keygen -t rsa -C danielkg91@gmail.com>
2017-03-06 10:25:36 +0000

    added test add enclosure and test number of enclosure to zootest

--- app/src/test/java/com/codeclan/com/javaproject/TestZoo.java
+++ app/src/test/java/com/codeclan/com/javaproject/TestZoo.java
@@ -14,13 +14,30 @@ public class TestZoo {
     Zoo zoo;
     Enclosure enclosure1;
     Enclosure enclosure2;
+    Enclosure enclosure3;
 
 
     @Before
     public void before() {
         zoo = new Zoo("Dans Mythical Zoo");
-        enclosure1 = new Enclosure("Dragon Lair");
+        enclosure1 = new Enclosure("The Lair");
         enclosure2 = new Enclosure("The Dark Deep");
+        enclosure3 = new Enclosure("The Labyrinth");
 
+    }
+
+    @Test
+    public void  testAddEnclosure() {
+        zoo.addEnclosure(enclosure1);
+        assertEquals(1, zoo.getNumberOfEnclosures());
+
+    }
+
+    @Test
+    public void testEnclosures() {
+        zoo.addEnclosure(enclosure1);
+        zoo.addEnclosure(enclosure2);
+        zoo.addEnclosure(enclosure3);
+        assertEquals(3, zoo.getNumberOfEnclosures());
     }
 }
